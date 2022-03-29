8c0f069ef3b3227ecbeb109415ac4df0fdf5894f
hcm444 <hcm444@gmail.com>
2022-03-29 05:49:19 -0700

    sss

--- matplot/views.py
+++ matplot/views.py
@@ -1,46 +1,33 @@
 import base64
 import io
 import urllib
-import matplotlib.pyplot as plt
-import pandas as pd
 # plotting
 import yfinance as yf
 # yahoo finance
 from django.shortcuts import render
 
 # for rendering
-stocks = []
+from matplotlib import pyplot as plt
+
+
 # create model here
 # multiple stocks
-stock1 = yf.download('MSFT', start='2022-01-01', end='2022-03-28', progress=False, auto_adjust=True)
-stock2 = yf.download('NVDA', start='2022-01-01', end='2022-03-28', progress=False, auto_adjust=True)
-stock3 = yf.download('APPL', start='2022-01-01', end='2022-03-28', progress=False, auto_adjust=True)
-stock4 = yf.download('TSLA', start='2022-01-01', end='2022-03-28', progress=False, auto_adjust=True)
 
 
 # get info
 # Create your views here.
 def home(request):
-    plt.plot(stock1)
-    plt.plot(stock2)
-    plt.plot(stock3)
-    plt.plot(stock4)
+    stock = yf.download('MSFT', start='2022-01-01', end='2022-03-28',)
+    plt.plot(stock)
+
     # plot them all
     # plotting
-    return render(request, 'home.html', {'data1': (plot())})
-    return render(request, 'home.html', {'data2': (plot())})
-    return render(request, 'home.html', {'data3': (plot())})
-    return render(request, 'home.html', {'data4': (plot())})
-    # print each on top
-    # return to home.html
 
-
-def plot():
     buffer = io.BytesIO()
     plt.gcf().savefig(buffer, format='png')
     # .png format
     buffer.seek(0)
-    string = base64.b64encode(buffer.read())
     # base 64 lib
-    uri = urllib.parse.quote(string)
-    return uri
+    return render(request, 'home.html', {'data': (urllib.parse.quote(base64.b64encode(buffer.read())))})
+    # print each on top
+    # return to home.html
